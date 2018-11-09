68d2898cdc3a6b51df85b6982a68711d2f035c59
Dpmon1 <43799381+Dpmon1@users.noreply.github.com>
2018-11-09 10:22:19 -0500

    Update addBorderFunction.m

--- addBorderFunction.m
+++ addBorderFunction.m
@@ -1,5 +1,5 @@
 function borderedImg = addBorderFunction (inputImg)
-    xmax, ymax = size(inputImg);
+    [xmax, ymax] = size(inputImg);
     borderedImg = zeros((xmax+2), (ymax+2), 3);
     borderedImg((2:(xmax+1)),(2:(ymax+1))) = inputImg;
 end
