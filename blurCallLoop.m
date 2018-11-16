92335a37829cb1c92a8391615b7e557cb93ddb02
Dpmon1 <43799381+Dpmon1@users.noreply.github.com>
2018-11-16 11:18:54 -0500

    Fixed some weird errors (missing code)
    As in title

--- blurCallLoop.m
+++ blurCallLoop.m
@@ -1,8 +1,7 @@
 function blurredMatrix = blurCallLoop (blurInputMatrix, blurAmount)
-
-    while i <= (xmax+2)
-        while j <= (ymax+2)
-            blurInputMatrix = borderedImg((i-1):(i+1);(j-1):(j+1));
+    while i <= (xmax+(2*blurAmount))
+        while j <= (ymax+(2*blurAmount))
+            blurInputMatrix = borderedImg((i-blurAmount):(i+blurAmount);(j-blurAmount):(j+blurAmount));
             blurredMatrix = blurFunction(blurInputMatrix)
         end
     end
