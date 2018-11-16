25fb7c0d66f28e5494c4a2896c23a4d629e32614
Dpmon1 <43799381+Dpmon1@users.noreply.github.com>
2018-11-16 11:25:17 -0500

    A few more bugs I missed in the last update
    As in name

--- blurCallLoop.m
+++ blurCallLoop.m
@@ -1,8 +1,10 @@
-function blurredMatrix = blurCallLoop (blurInputMatrix, blurAmount)
+function blurredMatrix = blurCallLoop (borderedMatrix, blurAmount)
+    i = 1;
+    j = 1;
     while i <= (xmax+(2*blurAmount))
         while j <= (ymax+(2*blurAmount))
-            blurInputMatrix = borderedImg((i-blurAmount):(i+blurAmount);(j-blurAmount):(j+blurAmount));
-            blurredMatrix = blurFunction(blurInputMatrix)
+            blurInputMatrix = borderedMatrix((i-blurAmount):(i+blurAmount);(j-blurAmount):(j+blurAmount));
+            blurredMatrix(i,j) = blurFunction(blurInputMatrix);
         end
     end
 end
--- blurMain.m
+++ blurMain.m
@@ -1,8 +1,8 @@
-function (output) = blurMain [inputImg, blurAmount]
+function (blurredMatrix) = blurMain [inputImg, blurAmount]
     %gui input: if light blur then 1 heavy then 2
     xmax, ymax = size(inputImg);
     for i = (1:blurAmount)
         borderedMatrix = addBorderFunction(inputImg);
     end
-    output = blurCallLoop(borderedMatrix, blurAmount);
+    blurredMatrix = blurCallLoop(borderedMatrix, blurAmount);
 end
